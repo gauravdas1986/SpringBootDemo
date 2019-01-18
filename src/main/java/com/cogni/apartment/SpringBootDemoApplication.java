c5e34a5d9d0ba76a4a57c4f6e5cd6e9ca5e840ab
729485 <729485@PC394403.cts.com>
2019-01-18 18:47:16 +0530

    batch

--- src/main/java/com/cogni/apartment/SpringBootDemoApplication.java
+++ src/main/java/com/cogni/apartment/SpringBootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.cogni.SpringBootDemo;
+package com.cogni.apartment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
